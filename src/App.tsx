0c2c006c44b0d307bd0b247347cd683dfce7c66c
Sachin <sachinbaluragi2.0@gmail.com>
2024-04-11 23:47:48 +0530

    Hello World App for Artspot

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HelloWorld from './HelloWorld';
+
+const App: React.FC = () => {
+  return (
+    <h1 className="App">
+      <HelloWorld />
+    </h1>
+  );
+};
+
+export default App;
---
+++ src/HelloWorld.tsx
@@ -0,0 +1,5 @@
+const HelloWorld: React.FC = () => {
+    return <div>Hello World</div>;
+  };
+  
+  export default HelloWorld;
